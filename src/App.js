416fc26c6e244fe97d4aecc3c5e7d1f0b37bfda1
unknown <moh.abouelsaad@gmail.com>
2023-01-03 13:47:43 +0200

    ff

--- src/App.js
+++ src/App.js
@@ -3,78 +3,91 @@ import './App.css';
 import React from 'react'
 
 class App extends React.Component { 
-      nums = [];
+      nums = "";
      operation
   constructor(props){ 
     super(props) 
  
     // Set initial state 
-    this.state = {sum: 0};
+    this.state = {nums: ""};
    
         
     // Binding this keyword 
     this.updateState = this.updateState.bind(this)
     }
     updateState = (p) => {
-      this.setState({sum: p});
-    }
+      if(!isNaN(p)){
+        this.setState({nums: this.state.nums+""+p});
 
-
-  addnum(props) {
-    this.nums.push(props)
+      }else{
+      this.setState({nums: this.state.nums+" "+p+" "});
     }
-   op(props) {
-    this.operation = props
   }
+
+
+    addnum(props) {
+      this.updateState(props)
+        }
+
    sum() {
-    if(this.operation == "+"){
-      this.updateState(parseInt(this.nums[this.nums.length-2])+parseInt(this.nums[this.nums.length-1]))
-      console.log(this.state.sum)
-    }else if(this.operation == "-"){
-      this.updateState(parseInt(this.nums[this.nums.length-2])-parseInt(this.nums[this.nums.length-1]))
-      console.log(this.state.sum)   
-     }
-    else if(this.operation == "*"){
-      this.updateState(parseInt(this.nums[this.nums.length-2])*parseInt(this.nums[this.nums.length-1]))
-      console.log(this.state.sum)   
-     }
-    else{
-      this.updateState(parseInt(this.nums[this.nums.length-2])/parseInt(this.nums[this.nums.length-1]))
-      console.log(this.state.sum)  
-      }
+    const arr =  this.state.nums.split(" ");
+    for(var x=0;x<=arr.length;x++){
+      var num = 0
+      var s = 0
+      if(!isNaN(arr[x])){
+        num = parseInt(arr[x])
+      }else{
+        if(arr[x]==="+"){
+          s = parseInt(arr[x-1])+parseInt(arr[x+1])
+          x+=2
+            }else if(arr[x]==="*"){
+              s = parseInt(arr[x-1])*parseInt(arr[x+1])
+              x+=2
+            }else if(arr[x]==="/"){
+              s = parseInt(arr[x-1])/parseInt(arr[x+1])
+              x+=2
+        }
+        else if(arr[x]==="-"){
+          s = parseInt(arr[x-1])-parseInt(arr[x+1])
+          x+=2
+    }
+    alert(num)
+            }
+    }
+    this.setState({nums:s})
   }
   
   render(){
     return(
     <div>
-      <div class="result">
+      <div className="result">
 <span>
-       {this.state.sum}
+       {this.state.nums}
       
        </span></div>
-     <div class="flex-container">
+     <div className="flex-container">
   <div onClick={()=>this.addnum(1)}>1</div>
   <div onClick={()=>this.addnum(2)}>2</div>
   <div onClick={()=>this.addnum(3)}>3</div>  
-  <div onClick={()=>this.op("+")}>+</div>  
+  <div onClick={()=>this.addnum("+")}>+</div>  
 </div>
-<div class="flex-container">
+<div className="flex-container">
   <div onClick={()=>this.addnum(4)}>4</div>
   <div onClick={()=>this.addnum(5)}>5</div>
   <div onClick={()=>this.addnum(6)}>6</div>  
-  <div onClick={()=>this.op("-")}>-</div>  
+  <div onClick={()=>this.addnum("-")}>-</div>  
 </div>
-<div class="flex-container">
+<div className="flex-container">
   <div onClick={()=>this.addnum(7)}>7</div>
   <div onClick={()=>this.addnum(8)}>8</div>
   <div onClick={()=>this.addnum(9)}>9</div>  
-  <div onClick={()=>this.op("*")}>*</div>  
+  <div onClick={()=>this.addnum("*")}>*</div>  
 </div>
-<div class="flex-container">
+<div className="flex-container">
   <div onClick={()=>this.op(0)}>0</div>
   <div onClick={()=>this.op(0)}>C.E.</div>
   <div onClick={()=>this.sum()}>=</div>  
-  <div onClick={()=>this.op("/")}>/</div>  
+  <div onClick={()=>this.addnum("/")}>/</div>  
 </div>
 </div>
     )
